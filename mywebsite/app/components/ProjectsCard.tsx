01a29bb33df48ba0c8f01462245e460a1fbb7de4
Aleks <Alekskijewski88@gmail.com>
2024-05-29 20:31:01 +0100

    new stuff

---
+++ mywebsite/app/components/ProjectsCard.tsx
@@ -0,0 +1,84 @@
+'use client'
+import React, { useState } from 'react';
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Button,
+    Modal,
+    ModalContent,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+    Spacer,
+    useDisclosure
+} from '@nextui-org/react';
+
+interface ProjectCardProps {
+    title: string;
+    description: string;
+    imageUrl?: string;
+    githubLink: string;
+    demoLink?: string;
+}
+
+const ProjectCard: React.FC<ProjectCardProps> = ({ title, description, imageUrl, githubLink, demoLink }) => {
+    const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+    const handleGitHubClick = () => {
+        window.open(githubLink, '_blank');
+    };
+
+    const handleDemoClick = () => {
+        if (demoLink) {
+            window.open(demoLink, '_blank');
+        }
+    };
+
+    return (
+            <Card style={{ height: '400px', width: '400px', maxWidth: '100%' }}>
+                <CardHeader style={{fontSize: '35px'}}>{title}</CardHeader>
+                <CardBody style={{ fontSize: '20px', fontWeight: 'normal', overflow: 'hidden' }}>
+                    <p>{description.length > 50 ? description.substring(0, 290) + '...' : description}</p>
+                    {imageUrl && <img src={imageUrl} alt={title} style={{ maxWidth: '100%', height: 'auto' }} />}
+                    <Spacer y={5}/>
+                </CardBody>
+                
+                <CardFooter>
+                    <Button variant="ghost" color="success" onClick={handleGitHubClick}>
+                        GitHub
+                    </Button>
+                    {demoLink && (
+                        <>
+                            <Spacer x={2} />
+                            <Button variant="ghost" color="warning" onClick={handleDemoClick}>
+                                Demo
+                            </Button>
+                        </>
+                    )}
+                    <Spacer x={2} />
+                    <Button onPress={onOpen} className="max-w-fit">Read more</Button>
+                </CardFooter>
+                <Modal size="4xl" isOpen={isOpen} onOpenChange={onOpenChange} isDismissable={false} isKeyboardDismissDisabled={true}>
+                    <ModalContent style={{ backgroundColor: '#333', color: '#fff' }}>
+                        {(onClose) => (
+                            <>
+                                <ModalHeader>{title}</ModalHeader>
+                                <ModalBody>
+                                    <p style={{ color: '#fff' }}>{description}</p>
+                                </ModalBody>
+                                <ModalFooter>
+                                    <Button color="danger" variant="light" onPress={onClose}>
+                                        Close
+                                    </Button>
+                                </ModalFooter>
+                            </>
+                        )}
+                    </ModalContent>
+                </Modal>
+            </Card>
+    );
+};
+
+export default ProjectCard;
